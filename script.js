619378b7fa1a4b6181f724445c2b80e53acec147
Jake <jmp2814@gmail.com>
2022-07-05 17:27:07 -0400

    added # to hours for local storage to work

--- script.js
+++ script.js
@@ -11,15 +11,15 @@ $(document).ready(function(){
         localStorage.setItem(time, text);
     })
     // gets any desciption text from local storage and displays it
-    $("hour9 .description").val(localStorage.getItem("hour9"));
-    $("hour10 .description").val(localStorage.getItem("hour10"));
-    $("hour11 .description").val(localStorage.getItem("hour11"));
-    $("hour12 .description").val(localStorage.getItem("hour12"));
-    $("hour13 .description").val(localStorage.getItem("hour13"));
-    $("hour14 .description").val(localStorage.getItem("hour14"));
-    $("hour15 .description").val(localStorage.getItem("hour15"));
-    $("hour16 .description").val(localStorage.getItem("hour16"));
-    $("hour17 .description").val(localStorage.getItem("hour17"));
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
 
     function hourTracker() {
 
